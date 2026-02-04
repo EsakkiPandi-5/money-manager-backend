44082b4b8c2bb138fb8e70f5c68c70f0b83c189c
EsakkiPandi-5 <esakkipandi733@gmail.com>
2026-02-04 16:08:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  // 👇 CRITICAL: auto-approve preflight
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 // CORS FIRST
 app.use(cors());
 app.options("*", cors());
